55d59d4d15cbe7d169d921d30189e7253888e623
siwoon.yoon <siwoon@gmailcom>
2024-10-15 16:45:28 +0900

    :zap:제품리스트불러오기 구현중

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// ------------------------ 토큰 보유 시 화면 변경 ----------------------------
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("token");
   const userInterOne = document.querySelector("#userinerface_first");
   if (token) {
     userInterOne.innerHTML = `<span>마이페이지</span>`;
   }
+});
+// ----------------------------------------------------------------------------
+
+// ------------------------ 상품 리스트 불러오기 ----------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://estapi.openmarket.weniv.co.kr/products/")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
