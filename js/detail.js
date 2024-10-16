230207df601140bbeec68cd720704eb723c3787d
siwoon.yoon <siwoon1602@gmail.com>
2024-10-16 12:44:33 +0900

    :zap:상품리스트 불러오기 동적으로 변경,스타일 수정,상품상세페이지 모달창 구현

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,4 @@
+//토큰을 보유하지않고 구매버튼을 누르면 모달창 보여줌
 const modal = document.querySelector(".modal");
 const sellBtn = document
   .querySelector(".now_sell")
@@ -7,3 +8,14 @@ const sellBtn = document
       modal.showModal();
     }
   });
+
+//모달창 바깥을 클릭하면 모달창이 닫히게함
+modal.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) modal.close();
+});
+
+const closeBtn = document.querySelector(".close_btn");
+closeBtn.addEventListener("click", () => {
+  console.log("닫혀라!");
+  modal.close();
+});
