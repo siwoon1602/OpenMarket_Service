0039b8e0fa225c866c050adc98547cd76b22b88c
siwoon1602 <siwoon1602@gmail.com>
2025-02-04 14:43:17 +0900

    ⚡️ 판매자상품 불러오는 api 통신 기능 추가

---
+++ js/sellerCenter.js
@@ -0,0 +1,49 @@
+import { API_BASE_URL } from "./config.js";
+
+window.addEventListener("pageshow", async () => {
+  const token = localStorage.getItem("token");
+  const storeName = localStorage.getItem("storeName");
+  try {
+    const response = await fetch(`${API_BASE_URL}${storeName}/products/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`데이터 로딩 실패: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const products = data.results;
+    const section = document.querySelector(".item_list_area");
+
+    const itemsHTML = products
+      .map(
+        (item) => `
+      <div class="item">
+        <ul class="product_info_text">
+          <img src="${item.image}" class="image" alt="${item.name}"/> 
+          <li>
+            <p>${item.name}</p>
+            <p>재고 ${item.stock}개</p>
+          </li>
+        </ul>
+        <ul class="edit_area">
+          <li>${item.price.toLocaleString()}원</li>
+          <li><button class="edit_item">수정</button></li>
+          <li><button class="delete_item">삭제</button></li>
+        </ul>
+      </div>
+    `
+      )
+      .join("");
+
+    section.innerHTML += itemsHTML;
+  } catch (error) {
+    alert("데이터 로딩 중 오류 발생: " + error.message);
+    throw error;
+  }
+});
