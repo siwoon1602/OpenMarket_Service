f4b50d13cd1b949c961f1c2eba59d50df52ba2a3
siwoon1602 <siwoon1602@gmail.com>
2025-02-05 15:02:27 +0900

    🎨 검색결과가 없는경우 alert 창이 나오고 return으로 종료됨

--- js/config.js
+++ js/config.js
@@ -18,7 +18,13 @@ searchBtn.addEventListener("click", async () => {
     if (!response.ok) {
       throw new Error(`검색 실패: ${response.status}`);
     }
+
     const data = await response.json();
+    if (data.count === 0) {
+      alert("검색 결과가 없습니다");
+      return;
+    }
     localStorage.setItem("searchData", JSON.stringify(data));
+    window.location.href = "./searchPage.html";
   } catch {}
 });
