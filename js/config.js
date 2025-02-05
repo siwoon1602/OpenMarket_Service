0bc19b0a39cfe087fe01dcb3016d822c6dbb3c5f
siwoon1602 <siwoon1602@gmail.com>
2025-02-05 15:02:55 +0900

    🔥 디버깅용 콘솔 삭제

--- js/config.js
+++ js/config.js
@@ -3,7 +3,6 @@ export const API_BASE_URL = "https://estapi.openmarket.weniv.co.kr/";
 const searchBtn = document.querySelector(".search-icon");
 
 searchBtn.addEventListener("click", async () => {
-  console.log("검색!");
   const searchInput = document.querySelector(".search_input");
 
   let searchValue = searchInput.value.trim("");
