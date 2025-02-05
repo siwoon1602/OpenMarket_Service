2b41bfa765ce1273f6f5f4348a1b6d5756d8e233
siwoon1602 <siwoon1602@gmail.com>
2025-02-05 14:59:28 +0900

    ⚡️ 검색버튼 클릭시 api 통신 후 리스폰값 로컬스토리지에 저장 기능 추가

--- js/config.js
+++ js/config.js
@@ -1,1 +1,24 @@
 export const API_BASE_URL = "https://estapi.openmarket.weniv.co.kr/";
+
+const searchBtn = document.querySelector(".search-icon");
+
+searchBtn.addEventListener("click", async () => {
+  console.log("검색!");
+  const searchInput = document.querySelector(".search_input");
+
+  let searchValue = searchInput.value.trim("");
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}products/?search=${searchValue}`,
+      {
+        method: "GET",
+        headers: {},
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`검색 실패: ${response.status}`);
+    }
+    const data = await response.json();
+    localStorage.setItem("searchData", JSON.stringify(data));
+  } catch {}
+});
