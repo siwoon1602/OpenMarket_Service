eb9487750bf5c48792d97ff42c768a19d23fd25c
siwoon1602 <siwoon1602@gmail.com>
2025-02-07 23:18:05 +0900

    🎨 payment 헤더 변경 로직 수정

--- js/payment.js
+++ js/payment.js
@@ -1,14 +1,14 @@
 import { API_BASE_URL } from "./config.js";
 window.addEventListener("pageshow", (e) => {
-  if (!token && userType !== "BUYER") {
-    alert("구매자 회원만 이용가능한 페이지 입니다!");
-    window.location.href = "./login.html";
-  }
-
   const token = localStorage.getItem("token");
   const userType = localStorage.getItem("userType");
   const userMenuTwo = document.querySelector("#userMenu2");
   const userMenu = document.querySelector(".user_menu");
+  if (!token || userType !== "BUYER") {
+    alert("구매자 회원만 이용가능한 페이지 입니다!");
+    window.location.href = "./login.html";
+    return;
+  }
 
   const handleLogout = (e) => {
     e.preventDefault();
